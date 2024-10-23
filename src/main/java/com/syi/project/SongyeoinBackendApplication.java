3af2c55423917d39af72fd9ece67f973dbc72ae9
RACHEL-2929 <sts06154@naver.com>
2024-10-23 21:26:53 +0900

    first commit

---
+++ src/main/java/com/syi/project/SongyeoinBackendApplication.java
@@ -0,0 +1,13 @@
+package com.syi.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SongyeoinBackendApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SongyeoinBackendApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/syi/project/SongyeoinBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.syi.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SongyeoinBackendApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
