16c32e18867ce5d5241017f6e0c4eca8d5282742
kimdoyeon <lemon8818@naver.com>
2024-11-14 16:45:50 +0900

    ✨feat: #3 등록 및 리스트 구현중 🐞fix: Club 엔티티 & DTO 수정

--- src/main/java/com/syi/project/club/entity/Club.java
+++ src/main/java/com/syi/project/club/entity/Club.java
@@ -1,9 +1,23 @@
 package com.syi.project.club.entity;
 
+import com.syi.project.club.dto.ClubDTO;
+import com.syi.project.club.dto.ClubRequestDTO;
+import com.syi.project.common.enums.CheckStatus;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Columns;
+import org.hibernate.validator.constraints.Length;
+import org.springframework.format.annotation.DateTimeFormat;
+
 import java.time.LocalDate;
 
 @Entity
+@Getter
+@NoArgsConstructor
 public class Club{
 
     @Id
@@ -12,21 +26,28 @@ public class Club{
 
     @Column(nullable = false)
     private String writer;  //작성자
-    @Column(nullable = false)
+
     private String checker; //승인자
+
+    @NotBlank(message = "참여자 명단을 입력해주세요")
     @Column(nullable = false)
     private String participants;    //참여자
 
-    @Column(length=500)
+    @Length(max = 100, message = "100자 이하로 입력해주세요")
     private String content; //내용
-    @Column(nullable = false)
+    @DateTimeFormat(pattern="yyyy-MM-dd")
     private LocalDate regDate;  //작성일
+    @DateTimeFormat(pattern="yyyy-MM-dd")
     @Column(nullable = false)
     private LocalDate studyDate;    //활동일
 
+    @Enumerated(EnumType.STRING)
     @Column(length=1, nullable = false)
-    private String checkStatus; //승인상태('Y', 'N', 'W')
-    private String checkCmt;    //승인메시지
+    private CheckStatus checkStatus; //승인상태('Y', 'N', 'W')
+    private String checkMessage;    //승인메시지
+
+    @Column(nullable = false)
+    private Long courseId;
 
     private String fileOriginalName;    //원본파일이름
     private String fileSavedName;   //저장된 파일이름
@@ -35,5 +56,33 @@ public class Club{
     private String filepath;    //파일경로
     private LocalDate fileRegDate;  //파일등록날짜
 
-    //수강코드번호? enroll
+    @Builder
+    public Club(Long courseId, String writer, String participants, String content,
+                LocalDate regDate, LocalDate studyDate, CheckStatus checkStatus, String checker, String checkMessage) {
+        this.courseId = courseId;
+        this.writer = writer;
+        this.participants = participants;
+        this.content = content;
+        this.regDate = regDate != null ? regDate : LocalDate.now();  // 기본 값 설정
+        this.studyDate = studyDate;
+        this.checkStatus = checkStatus != null ? checkStatus : CheckStatus.W;
+        this.checker = checker;
+        this.checkMessage = checkMessage;
+    }
+
+    // entity -> DTO
+    public ClubRequestDTO.ClubCreate toCreateDTO() {
+        return new ClubRequestDTO.ClubCreate(
+                this.courseId, this.writer, this.participants, this.content, this.regDate, this.studyDate, this.checkStatus);
+    }
+
+    public ClubRequestDTO.ClubUpdate toUpdateDTO() {
+        return new ClubRequestDTO.ClubUpdate(
+                this.participants, this.content, this.regDate, this.studyDate);
+    }
+
+    public ClubRequestDTO.ClubApproval toApprovalDTO() {
+        return new ClubRequestDTO.ClubApproval(
+                this.checker, this.checkStatus, this.checkMessage);
+    }
 }
