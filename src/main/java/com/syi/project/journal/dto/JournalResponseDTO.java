f290529e54be9c5b0fe73ca1f8df386c5ae0adca
yerin <greyblanket@naver.com>
2024-11-18 09:11:18 +0900

    ✨feat: 파일전체테이블

--- src/main/java/com/syi/project/journal/dto/JournalResponseDTO.java
+++ src/main/java/com/syi/project/journal/dto/JournalResponseDTO.java
@@ -1,3 +1,39 @@
+//package com.syi.project.journal.dto;
+//
+//import com.syi.project.journal.entity.Journal;
+//import java.time.LocalDateTime;
+//import lombok.Builder;
+//import lombok.Getter;
+//
+//// 일지 작성 후 조회할 때 사용될 객체
+//@Getter
+//public class JournalResponseDTO {
+//
+//  private Long id;
+//  private String memberName;
+//  private String courseName;
+//  private String title;
+//  private String content;
+//  private String fileUrl;
+//  private String fileName;
+//  private String fileSize;
+//  private LocalDateTime createdAt;
+//
+//  @Builder
+//  public JournalResponseDTO(Journal journal) {
+//    this.id = journal.getId();
+//    this.memberName = journal.getMember().getName();
+//    this.courseName = journal.getCourse().getName();
+//    this.title = journal.getTitle();
+//    this.content = journal.getContent();
+//    this.fileUrl = journal.getFileUrl();
+//    this.fileName = journal.getFileName();
+//    this.fileSize = journal.getFileSize();
+//    this.createdAt = journal.getCreatedAt();
+//  }
+//
+//}
+
 package com.syi.project.journal.dto;
 
 import com.syi.project.journal.entity.Journal;
@@ -5,13 +41,12 @@ import java.time.LocalDateTime;
 import lombok.Builder;
 import lombok.Getter;
 
-// 일지 작성 후 조회할 때 사용될 객체
 @Getter
 public class JournalResponseDTO {
 
   private Long id;
-  private String memberName;
-  private String courseName;
+  //private String memberName;
+  //private String courseName;
   private String title;
   private String content;
   private String fileUrl;
@@ -22,8 +57,8 @@ public class JournalResponseDTO {
   @Builder
   public JournalResponseDTO(Journal journal) {
     this.id = journal.getId();
-    this.memberName = journal.getMember().getName();
-    this.courseName = journal.getCourse().getName();
+    //this.memberName = journal.getMember().getName();
+    //this.courseName = journal.getCourse().getName();
     this.title = journal.getTitle();
     this.content = journal.getContent();
     this.fileUrl = journal.getFileUrl();
@@ -31,5 +66,4 @@ public class JournalResponseDTO {
     this.fileSize = journal.getFileSize();
     this.createdAt = journal.getCreatedAt();
   }
-
 }
