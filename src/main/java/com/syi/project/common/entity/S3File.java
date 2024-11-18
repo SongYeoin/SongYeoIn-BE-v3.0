3c4719144c0335ccabb8b2ea86d9ad9da7eb94a6
YJ-2 <dbwls7233@naver.com>
2024-11-18 15:31:10 +0900

    ✨feat: S3File 엔티티 설계
    S3File 엔티티 틀만 작성했습니다.

---
+++ src/main/java/com/syi/project/common/entity/S3File.java
@@ -0,0 +1,48 @@
+package com.syi.project.common.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Entity
+@Getter
+@EntityListeners(AuditingEntityListener.class)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class S3File {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(nullable = false)
+  private String s3Key; // S3에서 파일을 식별하는 키
+
+  @Column(nullable = false)
+  private String bucketName; // S3 버킷 이름
+
+  @Column(nullable = false)
+  private String fileName; // 원본 파일명
+
+  @Column(nullable = false)
+  private Long fileSize;
+
+  @Column(nullable = false)
+  private String fileUrl; // S3 접근 URL
+
+  @Builder
+  public S3File(String s3Key, String bucketName, String fileName, Long fileSize, String fileUrl) {
+    this.s3Key = s3Key;
+    this.bucketName = bucketName;
+    this.fileName = fileName;
+    this.fileSize = fileSize;
+    this.fileUrl = fileUrl;
+  }
+}
