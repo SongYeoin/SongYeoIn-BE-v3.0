cbbcf2dbe8963b09abd8f955094ef70472687132
kimdoyeon <lemon8818@naver.com>
2024-11-25 15:17:41 +0900

    🐞fix: #29 ClubFileRepository, AdminClubController 수정

--- src/main/java/com/syi/project/club/file/ClubFileRepository.java
+++ src/main/java/com/syi/project/club/file/ClubFileRepository.java
@@ -1,11 +1,12 @@
 package com.syi.project.club.file;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface ClubFileRepository {
+public interface ClubFileRepository extends JpaRepository<ClubFile, Integer> {
 
     List<ClubFile> findByClubId(Long id);
 }
