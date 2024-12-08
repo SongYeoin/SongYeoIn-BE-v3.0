9950d801cdf02cc5488d4fda9df7786477152a5f
kimdoyeon <lemon8818@naver.com>
2024-12-08 20:59:50 +0900

    🐞fix: 첨부파일 수정기능

--- src/main/java/com/syi/project/club/file/ClubFile.java
+++ src/main/java/com/syi/project/club/file/ClubFile.java
@@ -1,8 +1,12 @@
 package com.syi.project.club.file;
 
 import com.syi.project.club.entity.Club;
+import com.syi.project.file.entity.File;
 import jakarta.persistence.*;
-import lombok.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
@@ -14,19 +18,23 @@ public class ClubFile {
     @Column(name = "club_file_id")
     private Long id;
 
-    private Long clubId;
+    @OneToOne(fetch = FetchType.LAZY)  // ManyToOne에서 OneToOne으로 변경
+    @JoinColumn(name = "club_id")
+    private Club club;
 
-    private Long fileId;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "file_id")
+    private File file;
 
 
     @Builder
-    public ClubFile(Long clubId, Long fileId) {
-        this.clubId = clubId;
-        this.fileId = fileId;
+    public ClubFile(Club club, File file) {
+        this.club = club;
+        this.file = file;
     }
 
-    public void updateFileId(Long fileId) {
-        this.fileId = fileId;
+    public void updateFile(File file) {
+        this.file = file;
     }
 
 }
