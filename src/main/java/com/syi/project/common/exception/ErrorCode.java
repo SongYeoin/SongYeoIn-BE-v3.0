29755673a6be103087ab0aac1d2d45b5de94cf10
RACHEL-2929 <sts06154@naver.com>
2025-02-14 10:30:00 +0900

    🐞fix: 출석관련 ErrorCode 추가 및 메시지 수정

--- src/main/java/com/syi/project/common/exception/ErrorCode.java
+++ src/main/java/com/syi/project/common/exception/ErrorCode.java
@@ -61,15 +61,21 @@ public enum ErrorCode {
   JOURNAL_DOWNLOAD_FAILED("JOURNAL_009", "교육일지 일괄 다운로드에 실패했습니다.", HttpStatus.INTERNAL_SERVER_ERROR),
 
   // 출석 관련 에러
+  // 공통
   ATTENDANCE_NOT_IN_RANGE("ATTENDANCE_001", "학원 네트워크에서만 출석이 가능합니다.", HttpStatus.FORBIDDEN),
-   ATTENDANCE_ENTRY_NOT_ALLOWED("ATTENDANCE_002", "입실 가능 시간이 아닙니다. 교시 시작 전후 10분부터 가능합니다.", HttpStatus.FORBIDDEN),
-   ATTENDANCE_EXIT_NOT_ALLOWED("ATTENDANCE_008", "퇴실 가능 시간이 아닙니다. 교시 종료 전후 10분부터 가능합니다.", HttpStatus.FORBIDDEN),
-   //ATTENDANCE_ENTRY_TOO_EARLY("ATTENDANCE_002", "입실 시간이 너무 이릅니다. 교시 시작 10분 전부터 가능합니다.", HttpStatus.FORBIDDEN),
-  ATTENDANCE_ENTRY_TOO_LATE("ATTENDANCE_003", "입실 시간이 너무 늦었습니다. 교시 시작 후 10분 후까지 가능합니다.", HttpStatus.FORBIDDEN),
-  ATTENDANCE_EXIT_TOO_EARLY("ATTENDANCE_004", "퇴실 시간이 너무 이릅니다. 교시 종료 10분 전부터 가능합니다.", HttpStatus.FORBIDDEN),
-  ATTENDANCE_EXIT_TOO_LATE("ATTENDANCE_005", "퇴실 시간이 너무 늦었습니다. 교시 종료 후 10분 후까지 가능합니다.", HttpStatus.FORBIDDEN),
-  ATTENDANCE_ALREADY_ENTERED("ATTENDANCE_006","이미 입실했습니다.", HttpStatus.BAD_REQUEST),
-  ATTENDANCE_ALREADY_EXITED("ATTENDANCE_007","이미 퇴실했습니다.", HttpStatus.BAD_REQUEST),
+  // 입실
+  ATTENDANCE_ENTRY_NOT_ALLOWED("ATTENDANCE_ENTRY_001", "입실이 가능한 시간이 아닙니다", HttpStatus.FORBIDDEN),
+  ATTENDANCE_ENTRY_TOO_EARLY("ATTENDANCE_ENTRY_002", "입실 가능한 시간이 아닙니다. 교시 시작 40분 전부터 가능합니다.", HttpStatus.FORBIDDEN),
+  ATTENDANCE_ENTRY_TOO_LATE("ATTENDANCE_ENTRY_003", "입실 가능한 시간이 아닙니다. 마지막 교시 시작 후 20분 전까지 가능합니다.", HttpStatus.FORBIDDEN),
+  ATTENDANCE_ALREADY_ENTERED("ATTENDANCE_ENTRY_004","이미 입실했습니다.", HttpStatus.BAD_REQUEST),
+  ATTENDANCE_ENTRY_NOT_FIND_PERIOD("ATTENDANCE_ENTRY_005","입실 가능한 교시가 없습니다.",HttpStatus.BAD_REQUEST),
+
+  // 퇴실
+  ATTENDANCE_EXIT_NOT_ALLOWED("ATTENDANCE_EXIT_001", "퇴실 가능 시간이 아닙니다. 교시 종료 전후 10분부터 가능합니다.", HttpStatus.FORBIDDEN),
+  ATTENDANCE_EXIT_TOO_EARLY("ATTENDANCE_EXIT_002", "퇴실 시간이 너무 이릅니다. 교시 종료 10분 전부터 가능합니다.", HttpStatus.FORBIDDEN),
+  ATTENDANCE_EXIT_TOO_LATE("ATTENDANCE_EXIT_003", "퇴실 시간이 너무 늦었습니다. 교시 종료 후 10분 후까지 가능합니다.", HttpStatus.FORBIDDEN),
+  ATTENDANCE_ALREADY_EXITED("ATTENDANCE_EXIT_004","이미 퇴실했습니다.", HttpStatus.BAD_REQUEST),
+
 
 
   // 수강신청 관련 에러
