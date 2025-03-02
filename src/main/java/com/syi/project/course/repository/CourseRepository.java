ef7a997f047f534025ed979f2182df105db2ccc7
RACHEL-2929 <sts06154@naver.com>
2025-03-03 00:34:54 +0900

    🐞fix: 일괄 결석 처리에 사용한 쿼리문
    활성 상태인 모든 수업을 조회하는 쿼리와 특정 학생, 날짜, 교시에 대한 출석 기록을 조회하는 쿼리를 만들었습니다.

--- src/main/java/com/syi/project/course/repository/CourseRepository.java
+++ src/main/java/com/syi/project/course/repository/CourseRepository.java
@@ -3,6 +3,7 @@ package com.syi.project.course.repository;
 import com.syi.project.course.entity.Course;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -11,4 +12,10 @@ public interface CourseRepository extends JpaRepository<Course, Long>, CourseRep
   List<Course> findByDeletedByIsNull();
 
   Course findCourseById(Long courseId);
+
+  /**
+   * 활성 상태인 모든 수업을 조회합니다.
+   */
+  @Query("SELECT c FROM Course c WHERE c.status = 'ACTIVE'")
+  List<Course> findAllActiveCourses();
 }
